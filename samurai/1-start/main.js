7afe1e8ee9370ef11abeedbf8d41f0a64128fa04
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2023-08-07 13:51:05 +0200

    lesson1

---
+++ samurai/1-start/main.js
@@ -0,0 +1,10 @@
+console.log("start");
+
+//Instrukcja - najmniejsza składowa programu
+
+let result = 2 + 2;
+console.log(typeof result);
+console.log("test".toUpperCase());
+
+//Instrukcja moze ale nei musi czegos zwracac
+//jesli instrukcja cos zwraca to jest wyrazeniem
