dfd934c40d1798e77801dd580381022b2938f3ed
AniaPiWo <106525407+AniaPiWo@users.noreply.github.com>
2023-08-18 12:16:14 +0200

    op vol 1

---
+++ samurai/4-operators/main.js
@@ -0,0 +1,18 @@
+// OPERATOR
+//konstrukcja jezyka programowania, ktora wykonuje operacje na operandach (wartosciach argumentach) i zwraca jakas wartosc
+
+//konkatenacja - dodawanie stringow
+"2" + "2"; // "22"
+2 + "2"; //"22"
+2 + true; // 2+1 -> 3
+2 + false; //2+0 -> 2
+"2" + false; //"2false"
+2 * "2"; //4
+
+// operator moze byc znakiem, moze byc slowem (typeof, delete, new)
+
+let result;
+//Operator przypisania dwuargumentowy (binarny) - po jednym arg z jednej i drugiej strony operatora
+result = 1;
+//operator inkrementacji
+result++;
